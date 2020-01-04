9a960be951e1794f3ffed458403393d6c6e7742d
songhm <baliang616@163.com>
2020-01-04 16:03:31 +0800

    study for

--- iteration/iter.go
+++ iteration/iter.go
@@ -1,9 +1,10 @@
 package iteration
 
+const num = 5
 func Repeat(a string) string{
 	var repeated string
-	for i:=0; i<5; i++ {
-		repeated = repeated + a
+	for i:=0; i < num; i++ {
+		repeated += a
 	}
 	return repeated
 }
