640bbccdf884bfa7e1793feb2d209864306b6ad7
songhm <baliang616@163.com>
2020-01-04 16:25:49 +0800

    add example func and base test

--- iteration/iter_test.go
+++ iteration/iter_test.go
@@ -1,5 +1,6 @@
 package iteration
 
+import "fmt"
 import "testing"
 
 func TestIter(t *testing.T){
@@ -8,4 +9,29 @@ func TestIter(t *testing.T){
 	if repeated != expected{
 		t.Errorf("expected '%q' but got '%q'", expected, repeated)
 	}
+}
+
+
+
+// 示例函数, 如果去掉// Output那行，则示例函数则不会执行
+
+// === RUN   TestIter
+// --- PASS: TestIter (0.00s)
+// PASS
+// ok      github.com/banjin/iteration     0.640s
+
+// go test -v 
+func ExampleRepeat(){
+	expected := Repeat("a", 5)
+	fmt.Println(expected)
+	// Output: "aaaaa"
+	
+}
+
+
+//基准测试  go test -bench=.
+func BenchmarkRepeat(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Repeat("a", 1)
+    }
 }
