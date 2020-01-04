cb0c767b28c732d3e127f9b1d97506f4d0c2e8fc
songhm <baliang616@163.com>
2020-01-04 21:17:00 +0800

    使用类型别名

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -2,16 +2,17 @@ package pointers
 
 import "fmt"
 
+type Bitcoin int  //类型别名
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
  }
 
-func (w *Wallet) Deposit(a int){
+func (w *Wallet) Deposit(a Bitcoin){
 	fmt.Println("address of balance in Deposit is", &w.balance)
 	w.balance += a 
 }
 
-func (w *Wallet) Balance() int{
+func (w *Wallet) Balance() Bitcoin{
 	return w.balance
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,10 +5,10 @@ import "fmt"
 
 func TestWallet(t *testing.T){
 	wallet := Wallet{}
-	wallet.Deposit(10) //当调用一个函数或方法时，参数会被复制。
+	wallet.Deposit(Bitcoin(10)) //当调用一个函数或方法时，参数会被复制。
 	got := wallet.Balance()
 	fmt.Println("address of balance in test is", &wallet.balance)
-	want := 10
+	want := Bitcoin(10)
 	if got != want {
         t.Errorf("got %d want %d", got, want)
     }
