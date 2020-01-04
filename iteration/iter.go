fe52ac816c35641a142eea96c047c45faee6c68f
songhm <baliang616@163.com>
2020-01-04 16:13:00 +0800

    change repeat num

--- iteration/iter.go
+++ iteration/iter.go
@@ -1,9 +1,9 @@
 package iteration
 
 const num = 5
-func Repeat(a string) string{
+func Repeat(a string, n int) string{
 	var repeated string
-	for i:=0; i < num; i++ {
+	for i:=0; i < n; i++ {
 		repeated += a
 	}
 	return repeated
