c594d374a3d0b223caf1ca6590b6ec7940dbd824
songhm <baliang616@163.com>
2020-01-05 11:46:30 +0800

    使用自定义类型和方法

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,11 @@
 package maps
 
+type Dictionary map[string]string
+
 func Search(m map[string]string, key string) string{
 	return  m[key]
+}
+
+func (d Dictionary) Search(key string) string {
+	return d[key]
 }
