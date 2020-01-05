4e29f9680f546ccb9fd7ba19621fc4111071b421
songhm <baliang616@163.com>
2020-01-05 11:30:37 +0800

    添加辅助函数

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,8 +6,16 @@ func TestSearch(t *testing.T){
 	dictionary := map[string]string{"test": "this is just a test"}
 	got := Search(dictionary, "test")
 	want := "this is just a test"
-	if got != want {
-        t.Errorf("got '%s' want '%s' given, '%s'", got, want, "test")
-    }
 
+	assertStrings(t, got, want)
+
+}
+
+//辅助函数
+func assertStrings(t *testing.T, got, want string){
+	t.Helper() 
+
+	if got != want {
+        t.Errorf("got '%s' want '%s'", got, want)
+    }
 }
